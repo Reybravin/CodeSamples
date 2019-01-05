078c5120119a45805ce7004bc13354f635a02260
Rey Bravin <30288947+Reybravin@users.noreply.github.com>
2019-01-05 18:23:35 +0200

    Update navigator.swift

--- navigator.swift
+++ navigator.swift
@@ -5,3 +5,40 @@ protocol Navigator {
    func navigate(to destination: Destination)
 }
 
+class LoginNavigator: Navigator {
+
+   private weak var navigationController: UINavigationController?
+   private let viewControllerFactory: LoginViewControllerFactory
+   
+   init(navigationController: UINavigationController, viewControllerFactory: LoginViewControllerFactory) {
+      self.navigationController = navigationController
+      self.viewControllerFactory = viewControllerFactory
+   }
+   
+   func navigate(to destination: Destination) {
+      let viewController = makeViewController(for: destination)
+      navigationController?.pushViewController(viewController, animated: true)
+   }
+   
+   private func makeViewController(for destination : Destination) -> UIViewController {
+      switch destination {
+      case .loginCompleted(let user):
+         return viewControllerFactory.makeWelcomeViewController(forUser: user)
+         //return WelcomeViewController(user: user)
+      case .forgotPassword:
+         return viewControllerFactory.makePasswordResetViewController()
+         //return PasswordResteViewController()
+      case .signup:
+         viewControllerFactory.makeSignUpViewController()
+         //return SignUpViewController()
+      }
+      return UIViewController()
+   }
+   
+   enum Destination {
+      case loginCompleted(user: User)
+      case forgotPassword
+      case signup
+   }
+   
+}
