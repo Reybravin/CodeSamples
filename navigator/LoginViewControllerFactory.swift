94afb58d3d40b815a0c041441a2aa65a65875304
Rey Bravin <30288947+Reybravin@users.noreply.github.com>
2019-01-05 18:25:44 +0200

    Create LoginViewControllerFactory.swift

---
+++ navigator/LoginViewControllerFactory.swift
@@ -0,0 +1,13 @@
+//
+
+class LoginViewControllerFactory {
+   func makeWelcomeViewController(forUser: User) -> UIViewController {
+      return UIViewController()
+   }
+   func makePasswordResetViewController() -> UIViewController {
+      return UIViewController()
+   }
+   func makeSignUpViewController() -> UIViewController {
+      return UIViewController()
+   }
+}
