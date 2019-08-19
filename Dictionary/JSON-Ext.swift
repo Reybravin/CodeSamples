9f4c5b9bab772216d961f1438c17204192e133e0
Sergiy Sachuk <30288947+Reybravin@users.noreply.github.com>
2019-08-19 10:27:09 +0300

    Create JSON-Ext.swift

---
+++ Dictionary/JSON-Ext.swift
@@ -0,0 +1,10 @@
+extension Dictionary {
+    public var json: String? {
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: self, options: .prettyPrinted)
+            return String(bytes: jsonData, encoding: String.Encoding.utf8)
+        } catch {
+            return nil
+        }
+    }
+}
